51ce3df3087426cef0971b834b367046f8375dec
S K <84536129+shreyk04@users.noreply.github.com>
2021-08-28 01:25:42 -0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+      <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+     
+	 
+	 
+	 <style>
+	 .container{
+		 text-align:center;
+		 width:500px;
+		
+		 
+	 }
+	 #homePageContainer{
+		 margin-top:150px;
+		 
+	 }
+  html { 
+  background: url("img/bg1.jpg") no-repeat center center/cover; 
+  
+}
+body{
+	background:none;
+	color:#FFF;
+}
+#logInForm{
+	display:none;
+}
+.toggleForms{
+	font-weight:bold;
+	
+}
+#diary{
+	width:100%;
+	height:600px;
+	
+}
+#containerLoggedInPage{
+	margin-top:60px;
+	
+}
+.navbar-brand{
+	
+	font-weight:bold;
+}
+  </style>
+  </head>
+  <body>
